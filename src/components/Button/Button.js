fd3b5d6c1cfa33464e87568c54ebccf45910c063
Jonathan Wernersson <jonathan2008@live.se>
2019-03-22 11:33:10 +0100

    added id property to Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Button.module.css';
 
 const button = (props) => (
-    <button className={styles.Button} onClick={props.onClick} disabled={props.disabled}>
+    <button id={props.id} className={styles.Button} onClick={props.onClick} disabled={props.disabled}>
         {props.children}
     </button>
 );
